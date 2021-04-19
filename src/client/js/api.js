1cb1da348b497a4965b9fb5e2f6972809dd27ad0
Shubo Chao <shubochao@gmail.com>
2021-04-19 15:53:22 +0800

    Integrate weatherbit API

--- src/client/js/api.js
+++ src/client/js/api.js
@@ -1,10 +1,20 @@
 const GEONAMES_API_URL = 'http://api.geonames.org/search'
 const GEONAMES_USER_ACCOUNT = 'shubochao'
 
+const WEATHERBIT_API_URL = 'https://api.weatherbit.io/v2.0/forecast/daily'
+const WEATHERBIT_API_KEY = '7326d011a9e44dcd85007538c215a98c'
+
 const fetchLatLng = async (placeName) => {
   const res = await fetch(`${GEONAMES_API_URL}?username=${GEONAMES_USER_ACCOUNT}&q=${placeName}&type=json`)
   const json = await res.json()
   return json.geonames[0];
 }
 
-export {fetchLatLng}
+const fetchWeather = async (lat, lon, days) => {
+  const res = await fetch(`${WEATHERBIT_API_URL}?key=${WEATHERBIT_API_KEY}&lat=${lat}&lon=${lon}&days=${days}`);
+  const json = await res.json();
+  const data = json.data;
+  return data[data.length - 1];
+}
+
+export {fetchLatLng, fetchWeather}
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,4 @@
-import {fetchLatLng} from './api';
+import {fetchLatLng, fetchWeather} from './api';
 
 const formHandler = async (e) => {
   e.preventDefault();
@@ -6,12 +6,46 @@ const formHandler = async (e) => {
   const placeName = document.getElementById('placename').value;
   if (!placeName || !placeName.trim()) {
     alert('Please enter a place name!');
+    return;
   }
 
-  const latLng = await fetchLatLng(placeName);
+  const dateStr = document.getElementById('date').value;
+  if (!dateStr) {
+    alert('Please enter a date!');
+    return;
+  }
 
-  console.log(latLng);
-  // TODO: update UI
+  const [year, month, day] = dateStr.split('-').map(str => parseInt(str, 10));
+  const now = new Date();
+  const date = new Date();
+  date.setFullYear(year);
+  date.setMonth(month - 1);
+  date.setDate(day);
+
+  // verify future date
+  if (date <= now) {
+    alert('Please enter a future date!');
+    return;
+  }
+
+  const daysFromNow = (date - now) / (24 * 60 * 60 * 1000);
+  if (daysFromNow > 16) {
+    alert('Can only get the weather within 16 days!');
+    return;
+  }
+
+  try {
+    // Get lat and lng
+    const latLng = await fetchLatLng(placeName);
+
+    // fetch weather forecast
+    const weatherRes = await fetchWeather(latLng.lat, latLng.lng, daysFromNow);
+    console.log(weatherRes);
+
+    // TODO: update UI
+  } catch (e) {
+    // TODO: show error message on UI
+  }
 }
 
 export { formHandler }
